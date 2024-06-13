f5dfa3d6ade33faa59bd69586fd5d3fe1ae25394
Adrian333Dev <adrian333dev@gmail.com>
2024-06-14 00:41:57 +0400

    save changes

--- src/auth/interfaces/index.ts
+++ src/auth/interfaces/index.ts
@@ -1,28 +1,7 @@
 import { User, Organization, Employee, Prisma } from '@prisma/client';
 
-export interface IEmployeeProfile
-  extends Pick<Employee, 'empId' | 'orgId' | 'role'> {}
-
-export interface IAccessTokenPayload {
-  userId: number;
-  email: string;
-}
-
-export interface IRefreshTokenPayload extends IAccessTokenPayload {
-  refreshTokenId: string;
-}
-
-export interface IUserOutput extends Omit<User, 'password'> {}
-
-export interface IUserWithEmployeeProfiles
-  extends Pick<
-    Prisma.UserGetPayload<{
-      include: {
-        employeeProfiles: { select: { empId: true; orgId: true; role: true } };
-      };
-    }>,
-    'userId' | 'email' | 'employeeProfiles'
-  > {}
+// export interface IEmployeeProfile
+//   extends Pick<Employee, 'empId' | 'orgId' | 'role'> {}
 
 // export interface IUserProfile
 //   extends Omit<
@@ -34,24 +13,43 @@ export interface IUserWithEmployeeProfiles
 //     'password'
 //   > {}
 
-// With Organization Details
-export interface IUserProfile
-  extends Omit<
-    Prisma.UserGetPayload<{
-      include: {
-        employeeProfiles: { select: { empId: true; org: { select: { orgId: true; name: true } }; role: true } };
-      };
-    }>,
-    'password'
-  > {}
+// export interface IUserWithEmployeeProfiles
+//   extends Pick<
+//     Prisma.UserGetPayload<{
+//       include: {
+//         employeeProfiles: { select: { empId: true; orgId: true; role: true } };
+//       };
+//     }>,
+//     'userId' | 'email' | 'employeeProfiles'
+//   > {}
+
+export interface IAccessTokenPayload {
+  userId: number;
+  email: string;
+}
 
 export interface ITokens {
   accessToken: string;
   refreshToken: string;
 }
 
-const test: IUserWithEmployeeProfiles = {
-  userId: 1,
-  email: 'test',
-  employeeProfiles: [],
-};
+export interface IRefreshTokenPayload extends IAccessTokenPayload {
+  refreshTokenId: string;
+}
+
+
+export interface IUserProfile
+  extends Omit<
+    Prisma.UserGetPayload<{
+      include: {
+        employeeProfiles: {
+          select: {
+            empId: true;
+            org: { select: { orgId: true; name: true } };
+            role: true;
+          };
+        };
+      };
+    }>,
+    'password'
+  > {}
